849c37cac0fc5da411d63d3034395c56760f7f46
Magnus Lund <themlund98@gmail.com>
2023-03-10 10:17:39 +0100

    Refactored unit tests

--- contracts/MessageControlable.sol
+++ contracts/MessageControlable.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.4.16 <0.9.0;
 
 contract MessageControlable {
   modifier onlyContract() {
-    require(msg.sender != tx.origin, "Call only accesible from smart contract");
+    require(msg.sender != tx.origin, "Call only accessible from smart contract");
     _;
   }
 }
