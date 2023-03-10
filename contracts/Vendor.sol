0b83056ad61e7e9a2d5f433b73c912d3ffbdf098
Jannik Sommer <jannik.sommer@sommerhouse.net>
2023-03-10 10:34:37 +0100

    NatSpec update

--- contracts/Vendor.sol
+++ contracts/Vendor.sol
@@ -5,7 +5,7 @@ import "./AnnouncementService.sol";
 import "./IdentifierIssuerService.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/// @title Smart contract template for vendors. cl
+/// @title Smart contract template for vendors.
 /// @author Jannik Lucas Sommer & Magnus Mølgaard Lund.
 /// @notice The smart contract has the necessary functionallity to interact 
 ///         with the SENTINEL system and it's services. 
@@ -22,15 +22,26 @@ contract Vendor is Ownable {
     vendorId = _IIS.registerVendor();
   }
 
+  /// @notice Retrieves a vulnerability identifier from the Identifier Issuer Service. 
+  /// @dev The smart contract must be registered as a vendor (get vendoerId from constructor). 
+  /// @return String of the generated vulnerability identifier. 
   function getVulnerabilityId() onlyOwner public returns (string memory) {
     require(vendorId != 0, "Function only available with a vendor id");
     return _IIS.requestVulnerabilityIdentifier();
   }
   
+  /// Calls function on the Announcement Service which emits an event. 
+  /// @dev This function will automatically get a new vulnerability identifier. 
+  /// @param productId One or more product identifiers separated by comma.
+  /// @param documentLocation Location for the security advisory (i.e. IPFS CID). 
   function announceNewAdvisory(string memory productId, string memory location) onlyOwner public {
     _AS.announceNewAdvisory(getVulnerabilityId(), productId, location);
   }
 
+  /// @notice Calls function on the Announcement Service which emits an event. 
+  /// @param vulnerabilityId A vulnerability identifier for the vulnerability.
+  /// @param productId One or more product identifiers separated by comma.
+  /// @param documentLocation Location for the security advisory (i.e. IPFS CID). 
   function announceUpdatedAdvisory(string memory vulnerabilityId, string memory productId, string memory location) onlyOwner public {
     _AS.announceUpdatedAdvisory(vulnerabilityId, productId, location);
   }
