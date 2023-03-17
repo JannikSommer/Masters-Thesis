3cb5036cd2b9a8e3ec8cb76f4c315e53df029d9e
Jannik Sommer <jannik.sommer@sommerhouse.net>
2023-03-17 13:52:23 +0100

    Fixed deployment

--- migrations/2_deploy_migration.js
+++ migrations/2_deploy_migration.js
@@ -7,9 +7,10 @@ var exp1 = artifacts.require("./experiments/FirstExepriment.sol");
 
 module.exports = function(deployer) {
     deployer.deploy(exp1);
-    deployer.deploy(as);
-    deployer.deploy(iis).then( () => {
-        deployer.deploy(vendor, "SommerSoftware Inc.", as.address, iis.address);
-    })
+    deployer.deploy(as).then( () => {
+        deployer.deploy(iis).then( () => {
+            deployer.deploy(vendor, "SommerSoftware Inc.", as.address, iis.address);
+        });
+    });
     deployer.deploy(private);
 };
