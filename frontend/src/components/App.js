fa5f77645d60e3a53feb4d3ba0271f7d62ff60fb
Jannik Sommer <jannik.sommer@sommerhouse.net>
2023-03-20 09:49:59 +0100

    Setup IPFS node in frontend
    - Set up IPFS node to be used in the frontend to get information.
    - Added conditional rendering to wait for IPFS to be created.

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,21 +1,41 @@
 import { Route, Routes } from "react-router-dom"
 import Container from 'react-bootstrap/Container';
-
 import VulnerabilityTable from './VulnerabilityTable';
 import Announcement from './Announcement';
 import Settings from './Settings';
 import Accounts from './Accounts';
+import Spinner from 'react-bootstrap/Spinner';
+
+import { useEffect, useState } from "react";
+import * as IPFS from 'ipfs-core';
 
 function App() {
+  const [ipfs, setIpfs] = useState();
+
+  async function loadIpfs() {
+    var result = await IPFS.create({ repo: '/var/ipfs/data' });
+    setIpfs(result);
+  }
+
+  useEffect(() => {
+    loadIpfs();
+  }, []);
+
   return (
     <div>
       <Container>
-        <Routes>
-          <Route exact path='/' Component={VulnerabilityTable} />
-          <Route path='announcement' Component={Announcement} />
-          <Route path='Settings' Component={Settings} />
-          <Route path='Accounts' Component={Accounts} />
-        </Routes>
+        {ipfs === undefined
+        ? <Spinner animation="border" role="status"  style={{ width: "4rem", height: "4rem", position: "absolute", top: "20%", left: "50%" }}>
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+        : 
+          <Routes>
+            <Route exact path='/' Component={() => <VulnerabilityTable ipfs={ipfs}/>}/>
+            <Route path='announcement' Component={Announcement} />
+            <Route path='settings' Component={Settings} />
+            <Route path='accounts' Component={Accounts} />
+          </Routes>
+        }
       </Container>
     </div>
   )
