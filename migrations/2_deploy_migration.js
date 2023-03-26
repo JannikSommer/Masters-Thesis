f401b77def3c7d9e3ea2b0dc1a859d9ff3c5c324
Jannik Sommer <jannik.sommer@sommerhouse.net>
2023-03-26 18:37:55 +0200

    Update migration script

--- migrations/2_deploy_migration.js
+++ migrations/2_deploy_migration.js
@@ -2,14 +2,32 @@ var vendor = artifacts.require("./Vendor.sol");
 var as = artifacts.require("./AnnouncementService.sol");
 var iis = artifacts.require("./IdentifierIssuerService.sol");
 var private = artifacts.require("./Private.sol");
-var exp1 = artifacts.require("./experiments/FirstExepriment.sol");
+
+module.exports = async function(deployer) {
+    // announcment service
+    await deployer.deploy(as);
+    const as_instanse = await as.deployed(); 
+
+    // identifier issuer service
+    await deployer.deploy(iis); 
+    const iis_instance = await iis.deployed(); 
+
+    // vendor 1
+    await deployer.deploy(vendor, "SommerSoftware Inc.", as_instanse.address, iis_instance.address); 
+    const vendor_instance1 = await vendor.deployed();
+    await vendor_instance1.announceNewAdvisory("CSAFPID-0001,CSAFPID-0002,CSAFPID-0003,CSAFPID-0004,CSAFPID-0005,CSAFPID-0006", "QmPQuXq1JuipvhLKdDz84eSM3tLbESjDAKeAHNzScjZz7Y"); 
+
+    // vendor 2
+    await deployer.deploy(vendor, "SommerSoftware Inc.", as_instanse.address, iis_instance.address); 
+    const vendor_instance2 = await vendor.deployed();
+    await vendor_instance2.announceNewAdvisory("CSAFPID-0001,CSAFPID-0002,CSAFPID-0003,CSAFPID-0004,CSAFPID-0005,CSAFPID-0006", "QmPQuXq1JuipvhLKdDz84eSM3tLbESjDAKeAHNzScjZz7Y"); 
 
 
-module.exports = function(deployer) {
-    deployer.deploy(exp1); 
-    deployer.deploy(as);
-    deployer.deploy(iis).then(() => {
-        deployer.deploy(vendor, "SommerSoftware Inc.", as.address, iis.address);
-    });
-    deployer.deploy(private);
+    await deployer.deploy(private); 
+    const private_instance = await private.deployed();
 };
+
+
+async function setupNetwork() {
+
+}
