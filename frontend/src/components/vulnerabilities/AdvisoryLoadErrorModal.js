8bb2958089cd208dcc6895d127b37b2e86d30339
Magnus Mølgaard Lund <themlund98@gmail.com>
2023-04-24 16:34:27 +0200

    SHA-256 hash is now verified on advisory download

--- frontend/src/components/vulnerabilities/AdvisoryLoadErrorModal.js
+++ frontend/src/components/vulnerabilities/AdvisoryLoadErrorModal.js
@@ -6,7 +6,7 @@ function AdvisoryLoadErrorModal({show, dismiss}) {
         <Modal size='lg' show={show} onHide={() => dismiss()}>
             <Modal.Header closeButton><Modal.Title>An error occurred!</Modal.Title></Modal.Header>
             <Modal.Body>
-                <p> An error occurred trying to read the selected security advisory, probably due to a encryption key mismatch. 
+                <p> An error occurred trying to read the selected security advisory, probably due to a encryption key mismatch or a hash mismatch. 
                     Update the public key on your contract and have your vendor re-announce the advisory. </p>
             </Modal.Body>
             <Modal.Footer>
