adfa933e6e86e31b72f72a9ada67128c8111e9b7
Magnus Mølgaard Lund <themlund98@gmail.com>
2023-05-02 20:39:35 +0200

    Updated Slither script to ignore OpenZeppelin

--- runSlither.sh
+++ runSlither.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p analysis/slitherResults
-slither . --ignore-compile --checklist > analysis/slitherResults/checklist.md
-slither . --ignore-compile
-slither . --ignore-compile --print human-summary
+slither . --ignore-compile --filter-paths openzeppelin --checklist > analysis/slitherResults/checklist.md
+slither . --ignore-compile --filter-paths openzeppelin 
+slither . --ignore-compile --filter-paths openzeppelin  --print human-summary
