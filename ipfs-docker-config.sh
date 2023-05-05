553fc7d81ed9058d3742a6cc063298f33aa43d6c
Jannik Sommer <jannik.sommer@sommerhouse.net>
2023-05-05 23:42:18 +0200

    Fix ipfs config

--- ipfs-docker-config.sh
+++ ipfs-docker-config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -ex
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"127.0.0.1:80\", \"http://localhost\"]"
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin "[\"*\"]"
 ipfs daemon
