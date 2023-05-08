dbb2371a3cb98ee82d86e8329f70fd38f42acc41
Jannik Sommer <jannik.sommer@sommerhouse.net>
2023-05-08 21:59:44 +0200

    Update publish.js

--- frontend/scripts/publish.js
+++ frontend/scripts/publish.js
@@ -1,6 +1,7 @@
 import {create, globSource} from "ipfs-http-client";
 
 const ipfs = await create({host: process.argv[2], protocol: "https"});
+console.log(ipfs);
 //const address = "ipfs/k51qzi5uqu5dmjabueumy621fbdczkie0z0sa1vq57kjet242lcum9opm1qvbl";
 
 //options specific to globSource
@@ -10,5 +11,7 @@ const globSourceOptions = { recursive: true };
 const addOptions = { pin: true, wrapWithDirectory: true, timeout: 10000 };
 
 var response = await ipfs.addAll(globSource(process.argv[3], globSourceOptions), addOptions);
+console.log(response);
+
 const res = await ipfs.name.publish("/ipfs/" + response[0].cid.toString());
 console.log(`https://gateway.ipfs.io/ipns/${res.name}`);
