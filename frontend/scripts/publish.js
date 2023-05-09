e1f2a464e487df252d0e0c001e98340b962d677d
Jannik Sommer <jannik.sommer@sommerhouse.net>
2023-05-09 22:02:35 +0200

    Update publish.js

--- frontend/scripts/publish.js
+++ frontend/scripts/publish.js
@@ -7,13 +7,15 @@ const ipfs = await create({url: process.argv[2]});
 const globSourceOptions = { recursive: true };
   
 //example options to pass to IPFS
-const addOptions = { pin: true, wrapWithDirectory: true, timeout: 10000 };
+const addOptions = { pin: true, wrapWithDirectory: true, timeout: 60000 };
 
 var results = [];
 for await (const file of ipfs.addAll(globSource(process.argv[3].toString(), "**/*", globSourceOptions), addOptions)) {
     results.push(file);
 }
-console.log(results[0]);
-
-const res = await ipfs.name.publish("/ipfs/" + results[0].cid.toString());
-console.log(`https://gateway.ipfs.io/ipns/${res.name}`);
+for await (let result of results) {
+    if (result.path === '') { // this is the directory itself
+        const res = await ipfs.name.publish("/ipfs/" + results[0].cid.toString());
+        console.log(`https://gateway.ipfs.io/ipns/${res.name}`);
+    }
+}
